de5bae232c4cdebd2883eea4a77ef93ca4557719
xiaobofu <xiaobofu.maps@gmail.com>
2022-04-20 22:22:23 +0100

    3st

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 from matplotlib import pyplot as plt
+from scipy.spatial.transform import Rotation
 
 
 # points
@@ -10,35 +11,54 @@ class Point:
         self.y = y
         self.z = z
         self.cord = (x, y)
+        self.cord3d = (x, y, z)
         self.width = z
+
 
 # strokes
 class Strokes:
-    def __int__(self, points, parameters):
+    def __init__(self, points, mus, sigmas, deltas, alphas, t0s):
         self.points = points
-        self.parameters = parameters
+        self.mus = mus
+        self.sigmas = sigmas
+        self.deltas = deltas
+        self.alphas = alphas
+        self.t0s = t0s
 
-    def get_trejector(self):
-        for i in range(len(self.points) - 1):
-            trejectory = 0
+    def get_trejectory(self):
+        trejectory = []
+        for t in range(300):
+            point_cord = np.array([0.0, 0.0, 0.0])
+            for j in range(len(self.points) - 1):
+                cord = get_arc_cord(self.points[j], self.points[j+1], t/100, mu=self.mus[j], sigma=self.sigmas[j], delta=self.deltas[j], alpha=self.alphas[j], t0=self.t0s[j])
+                cord = np.array(cord)
+                point_cord += cord
+            point_cord += np.array(self.points[j].cord3d)
+            trejectory.append(point_cord)
         return trejectory
 
 
-
 if __name__ == "__main__":
-    def get_arc_cord(p_oirgin, p_end, t, mu=0, sigma=0, delta=0.001):
+    def get_arc_cord(p_oirgin, p_end, t, mu=0, sigma=0, delta=0.001, alpha=0, t0=0):
         norm = get_norm(p_oirgin, p_end)
         theta = get_theta(p_oirgin, p_end)
-        alpha = np.arcsin((p_end.z - p_oirgin.z) / norm)
         D = norm * delta / 2 / np.sin(delta/2)
-        t0 = 0
-        w = 1 / 2 * (1 + math.erf((np.log(t - t0) - mu) / (sigma * np.sqrt(2))))
+        if 0 >= t0:
+            w = 1 / 2 * (1 + math.erf((np.log(t - t0) - mu) / (sigma * np.sqrt(2))))
+        else:
+            w = 0
         # print(w)
         phi = theta - delta / 2 + delta * w
         # print(phi)
         dx, dy = get_displacement_cord(delta, D, theta, w, phi)
         dz = w * (p_end.width - p_oirgin.width)
-        return p_oirgin.x + dx, p_oirgin.y + dy, p_oirgin.z + dz
+
+        #rotation
+        axis = (np.array(p_end.cord3d) - np.array(p_oirgin.cord3d))/norm
+        v = [dx, dy, dz]
+        new_v = np.dot(rotation_matrix(axis, alpha), v)
+
+        return new_v[0], new_v[1], new_v[2]
 
     def get_norm(p_oirgin, p_end):
         return np.sqrt((p_oirgin.x - p_end.x) ** 2 + (p_oirgin.y - p_end.y) ** 2 + (p_oirgin.z - p_end.z) ** 2)
@@ -60,25 +80,45 @@ if __name__ == "__main__":
         return displacement_x, displacement_y
 
 
+    def rotation_matrix(axis, theta):
+        """
+        Return the rotation matrix associated with counterclockwise rotation about
+        the given axis by theta radians.
+        """
+        axis = np.asarray(axis)
+        axis = axis / math.sqrt(np.dot(axis, axis))
+        a = math.cos(theta / 2.0)
+        b, c, d = -axis * math.sin(theta / 2.0)
+        aa, bb, cc, dd = a * a, b * b, c * c, d * d
+        bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+        return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                         [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                         [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
 
 
+    p1 = Point(500, 500, 50)
+    p2 = Point(100, 100, 10)
+    p3 = Point(0, 200, 10)
 
-    p1 = Point(100, 100, 0)
-    p2 = Point(400, 450, 60)
-    p3 = []
-    for i in range(1000):
-        x, y, z = get_arc_cord(p1, p2, i/100, mu=0.1, sigma=0.4, delta=1)
-        p3.append(Point(x, y, z))
+    points = [p1, p2, p3]
+    mus = [0.1, 0.1]
+    sigmas = [0.4, 0.4]
+    deltas = [1, 0.1]
+    alphas = [-0.2, 0.2]
+    t0s = [0, 0]
+
+    stroke = Strokes(points, mus, sigmas, deltas, alphas, t0s)
+    trejectory = stroke.get_trejectory()
 
     plt.rcParams["figure.figsize"] = [8.00, 8.50]
     plt.rcParams["figure.autolayout"] = True
     x = [5]
     y = [5]
-    plt.xlim(0, 600)
-    plt.ylim(0, 600)
+    plt.xlim(-200, 600)
+    plt.ylim(-200, 600)
     plt.grid()
-    plt.plot(p1.x, p1.y, marker="o", markersize=p1.width, markeredgecolor="black", markerfacecolor="black")
-    plt.plot(p2.x, p2.y, marker="o", markersize=p2.width, markeredgecolor="black", markerfacecolor="black")
-    for i in range(1000):
-        plt.plot(p3[i].x, p3[i].y, marker="o", markersize=p3[i].width, markeredgecolor="black", markerfacecolor="black")
+    # plt.plot(p1.x, p1.y, marker="o", markersize=p1.width, markeredgecolor="black", markerfacecolor="black")
+    # plt.plot(p2.x, p2.y, marker="o", markersize=p2.width, markeredgecolor="black", markerfacecolor="black")
+    for i in trejectory:
+        plt.plot(i[0], i[1], marker="o", markersize=20, markeredgecolor="black", markerfacecolor="black")
     plt.show()
--- test.py
+++
@@ -1,3 +0,0 @@
-import numpy
-import math
-print(numpy.log(10))
